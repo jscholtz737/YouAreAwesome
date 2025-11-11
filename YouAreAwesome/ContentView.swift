99cf6db9ef0957168e5e3d196d722594d26f4e18
Joe <joescholtz@hotmail.com>
2025-11-11 11:25:20 -0600

    Adds interactive buttons

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -8,16 +8,38 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var message = "I am a programmer!"
+    
     var body: some View {
+        
         VStack {
+            
+            Spacer()
+            
             Image(systemName: "swift")
                 .resizable()
                 .scaledToFit()
                 .foregroundStyle(.orange)
-            Text("You Are Awesome!")
+                .frame(width: 200, height: 200)
+            Text(message)
                 .font(.largeTitle)
-                .fontWeight(.heavy)
-                .foregroundStyle(.red)
+                .fontWeight(.ultraLight)
+            
+            Spacer()
+            
+            HStack {
+                Button("Awesome!") {
+                    message = "Awesome!"
+                }
+                
+                Button("Great!") {
+                    message = "Great!"
+                }
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            .tint(.orange)
         }
         .padding()
     }
